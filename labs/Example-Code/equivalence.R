99cb4a92b61713d9242ca634a6d05904a501459e
scunning1975 <scunning@gmail.com>
2025-06-29 20:52:37 +0200

    Updated do file

--- labs/Example-Code/equivalence.R
+++ labs/Example-Code/equivalence.R
@@ -1,55 +1,70 @@
 # equivalence.R
 # author: Scott Cunningham
-# description: OLS and Manual equivalence in R
+# description: OLS and Manual are the same
 
+library(haven)
 library(dplyr)
 library(tidyr)
-library(lmtest)
-library(sandwich)
 library(fixest)
 
 # Load dataset
-castle <- haven::read_dta("https://github.com/scunning1975/mixtape/raw/master/castle.dta")
+castle <- read_dta("https://github.com/scunning1975/mixtape/raw/master/castle.dta")
 
-# Filter data
-castle_filtered <- castle %>%
+# Clean and prep
+castle <- castle %>%
   filter(!(effyear %in% c(2005, 2007, 2008, 2009))) %>%
-  mutate(post = ifelse(year >= 2006, 1, 0),
-         treat = ifelse(effyear == 2006, 1, 0)) %>%
+  mutate(
+    post = ifelse(year >= 2006, 1, 0),
+    treat = ifelse(effyear == 2006, 1, 0)
+  ) %>%
   filter(year %in% c(2005, 2006))
 
-# Manual DID calculation
-y11 <- mean(castle_filtered$l_homicide[castle_filtered$post == 1 & castle_filtered$treat == 1], na.rm = TRUE)
-y10 <- mean(castle_filtered$l_homicide[castle_filtered$post == 0 & castle_filtered$treat == 1], na.rm = TRUE)
-y01 <- mean(castle_filtered$l_homicide[castle_filtered$post == 1 & castle_filtered$treat == 0], na.rm = TRUE)
-y00 <- mean(castle_filtered$l_homicide[castle_filtered$post == 0 & castle_filtered$treat == 0], na.rm = TRUE)
+# Manual DiD Calculation
+manual <- castle %>%
+  group_by(treat, post) %>%
+  summarise(y = mean(l_homicide, na.rm = TRUE), .groups = "drop") %>%
+  pivot_wider(names_from = c(post), values_from = y, names_prefix = "post_") %>%
+  mutate(
+    did = (post_1 - post_0)[treat == 1] - (post_1 - post_0)[treat == 0]
+  )
 
-DID <- (y11 - y10) - (y01 - y00)
-cat("Manual DID estimate:", DID, "\n")
+cat("Manual DiD estimate:\n")
+print(manual$did[1])
 
 # Example 1: OLS regression with interaction
-ols_model <- lm(l_homicide ~ post * treat, data = castle_filtered)
-coeftest(ols_model, vcov = vcovCL, cluster = ~sid)
+mod1 <- feols(l_homicide ~ post * treat, cluster = ~sid, data = castle)
+summary(mod1)
 
-# Example 2: Two-way fixed effects (state and year fixed effects)
-fe_model <- feols(l_homicide ~ post:treat | sid + year, cluster = ~sid, data = castle_filtered)
-summary(fe_model)
+# Example 2: TWFE (state and year FE)
+mod2 <- feols(l_homicide ~ i(year) + post * treat | sid, cluster = ~sid, data = castle)
+summary(mod2)
 
 # Example 3: Long difference regression
-castle_wide <- castle_filtered %>%
+long_diff <- castle %>%
   select(sid, year, l_homicide, treat) %>%
-  pivot_wider(names_from = year, values_from = l_homicide, names_prefix = "homicide_") %>%
-  mutate(diff = homicide_2006 - homicide_2005)
+  pivot_wider(names_from = year, values_from = l_homicide, names_prefix = "y") %>%
+  mutate(diff = y2006 - y2005)
 
-long_diff_model <- lm(diff ~ treat, data = castle_wide)
-coeftest(long_diff_model, vcov = vcovCL, cluster = ~sid)
+mod3 <- feols(diff ~ treat, cluster = ~sid, data = long_diff)
+summary(mod3)
 
+# Weighted models
 
-# Population weights
+# OLS with weights
+mod1w <- feols(l_homicide ~ post * treat, cluster = ~sid, weights = ~popwt, data = castle)
+summary(mod1w)
 
-# Example 1: Weighted OLS regression
+# TWFE with weights
+mod2w <- feols(l_homicide ~ i(year) + post * treat | sid, cluster = ~sid, weights = ~popwt, data = castle)
+summary(mod2w)
 
-# Example 2: Two-way fixed effects with population weights
+# Weighted long difference
+long_diff_w <- castle %>%
+  select(sid, year, l_homicide, popwt, treat) %>%
+  pivot_wider(names_from = year, values_from = c(l_homicide, popwt)) %>%
+  mutate(diff = l_homicide_2006 - l_homicide_2005)
 
-# Example 3: Weighted long difference regression
-
+mod3w <- feols(diff ~ treat, cluster = ~sid, weights = ~popwt_2005, data = long_diff_w)
+summary(mod3w)
+mod3w <- feols(diff ~ treat, cluster = ~sid, weights = ~popwt_2005, data = long_diff_w)
+summary(mod3w)
